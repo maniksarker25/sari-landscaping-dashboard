eb966b2fed618a1e8e19d1c619e718a63b8c73e6
manik <maniksarker652@gmail.com>
2026-08-15 13:35:12 +0600

    add more things

--- src/redux/baseApis.ts
+++ src/redux/baseApis.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "./store";
-
+//
 const baseApis = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
